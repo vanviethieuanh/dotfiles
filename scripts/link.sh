8a15940837c8e46680cda581427272fe22671fc2
Văn Viết Hiếu Anh <29355962+vanviethieuanh@users.noreply.github.com>
2025-04-28 23:29:40 +0700

    Better link logic

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,15 +1,36 @@
 #!/bin/zsh
 set -euo pipefail
 
+cwd="$(pwd)"
 file_links="$(pwd)/links/file.tsv"
 dir_links="$(pwd)/links/dir.tsv"
 
-# Loop through each line in file_links.txt
+# Function to safely create a symlink
+create_link() {
+    local from="$1"
+    local to="$2"
+
+    if [ -L "$to" ] && [ "$(readlink "$to")" = "$from" ]; then
+        echo "Already linked: $to -> $from, skipping."
+    else
+        mkdir -p "$(dirname "$to")" # Ensure destination folder exists
+        ln -sf "$from" "$to"
+        echo "Created link: $from -> $to"
+    fi
+}
+
+# Loop through each line in file.tsv
 while IFS= read -r file; do
-  from="$(pwd)/$file"
-  to="$HOME/$file"
+    from="$cwd/$file"
+    to="$HOME/$file"
 
-  ln -sf $from $to 
-  echo "Created file link: $from -> $to"
-done < "$file_links"
+    create_link "$from" "$to"
+done <"$file_links"
 
+# Loop through each line in dir.tsv
+while IFS=' ' read -r name dest; do
+    from="$cwd/$name"
+    to="$HOME/$dest"
+
+    create_link "$from" "$to"
+done <"$dir_links"
