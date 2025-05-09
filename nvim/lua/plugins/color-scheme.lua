fd9eb58dfa89332b5d0a7c688cda790cf6ac19aa
Văn Viết Hiếu Anh <anh.van@codelink.io>
2025-05-09 18:28:22 +0700

    Refactor

--- nvim/lua/plugins/color-scheme.lua
+++ nvim/lua/plugins/color-scheme.lua
@@ -1,11 +1,14 @@
+local color_scheme = 'gruvbox-material'
+local color_name = color_scheme:match '[^/]+$'
+
 return {
-  'sainnhe/gruvbox-material',
+  color_scheme,
   lazy = false,
   priority = 1000,
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.gruvbox_material_background = 'hard'
-    vim.cmd.colorscheme 'gruvbox-material'
+    vim.cmd.colorscheme(color_name)
   end,
 }
