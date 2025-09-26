97b4a8ec879f364b1d64714b0fb126fa30b8c5cf
Văn Viết Hiếu Anh <anh.van@codelink.io>
2025-09-26 13:35:10 +0700

    Remove session lens

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -4,13 +4,10 @@ return {
   lazy = false,
   config = function()
     local auto_session = require 'auto-session'
-    local auto_session_lens = require 'auto-session.session-lens'
 
     auto_session.setup {
       suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
     }
-
-    vim.keymap.set('n', '<leader>ls', auto_session_lens.search_session, { desc = '[l]ist all [s]essions.' })
   end,
   ---enables autocomplete for opts
   ---@module "auto-session"
