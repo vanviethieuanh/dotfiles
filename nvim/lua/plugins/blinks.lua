76ec0e2a2c275185c01105ede7edd0213b8dd7ea
Văn Viết Hiếu Anh <29355962+vanviethieuanh@users.noreply.github.com>
2025-10-01 00:06:16 +0700

    clean up

--- nvim/lua/plugins/blinks.lua
+++ nvim/lua/plugins/blinks.lua
@@ -11,9 +11,6 @@ return {
       'L3MON4D3/LuaSnip',
       version = '2.*',
       build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
         end
@@ -23,17 +20,18 @@ return {
         -- `friendly-snippets` contains a variety of premade snippets.
         --    See the README about individual language/framework/plugin snippets:
         --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+          end,
+        },
       },
       opts = {},
     },
     'folke/lazydev.nvim',
   },
+
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
--- nvim/lua/plugins/lua-snip.lua
+++ nvim/lua/plugins/lua-snip.lua
@@ -2,10 +2,7 @@ return {
   'L3MON4D3/LuaSnip',
   dependencies = { 'rafamadriz/friendly-snippets' },
   config = function()
-    -- Load vscode-style snippets (optional, gives you many defaults)
     require('luasnip.loaders.from_vscode').lazy_load()
-
-    -- Load your Lua snippets from lua/snippets/
     require('luasnip.loaders.from_lua').lazy_load {
       paths = { vim.fn.stdpath 'config' .. '/lua/snippets' },
     }
