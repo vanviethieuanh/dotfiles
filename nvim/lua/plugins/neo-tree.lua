621c590cf107287bf44a6579bd193b40341e9095
Văn Viết Hiếu Anh <29355962+vanviethieuanh@users.noreply.github.com>
2025-10-01 01:49:44 +0700

    clean

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,7 @@ return {
     { '3rd/image.nvim', opts = {} }, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   lazy = false, -- neo-tree will lazily load itself
+
   ---@module "neo-tree"
   ---@type neotree.Config?
   opts = {
