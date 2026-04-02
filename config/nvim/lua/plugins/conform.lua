e50e63c95bd24c7ea13c4f7a684acaa5200e69ba
Văn Viết Hiếu Anh <avan.contractor@adb.org>
2026-04-02 15:27:00 +0700

    Prettier

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -33,9 +33,11 @@ return { -- Autoformat
       -- Conform can also run multiple formatters sequentially
       python = { 'isort', 'black' },
       nix = { 'alejandra' },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { 'prettier' },
+      typescriptreact = { 'prettier' },
+      javascript = { 'prettier' },
+      javascriptreact = { 'prettier' },
+      json = { 'prettier' },
     },
   },
 }
